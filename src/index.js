3f8e17d92865785e1e23f85958f2f38e45efef9f
Yan Velasco <yancarlosduartevelasco@gmail.com>
2023-08-03 19:56:49 -0300

    Header finish

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './pages';
+import "./styles.css"
+import App from './pages/Home';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>
 );
