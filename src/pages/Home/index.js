e409081d6cdc36727708e76fd1b79605b2f217b9
Yan Velasco <yancarlosduartevelasco@gmail.com>
2023-08-03 22:29:18 -0300

    Finish profile section

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,22 +1,35 @@
 import { Header } from "../../components/Header";
-import backgroundImage from '../../assets/img/background.png';
+import backgroundImage from "../../assets/img/background.png";
 
-import './styles.css'
+import "./styles.css";
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <div className="content">
-        <img src = {backgroundImage} alt = "GitHub img" className="background-image"/>
-        <div className="Informations">
-          <div>
-            <input name="user" placeholder="@user" className ="user"/>
-            <button className="search">
-              Search
-            </button>
-          </div>
-        </div>
-      </div>
+      <Header />
+      <main className="content">
+        <img
+          src={backgroundImage}
+          alt="GitHub img"
+          className="background-image"
+        />
+        <section className="Informations">
+          <article>
+            <input name="user" placeholder="@user" className="user" />
+            <button className="search"><p>search</p></button>
+          </article>
+          <article className="profile">
+            <img src={backgroundImage} name="profile" alt="Profile img" className="profile-img"/>
+            <div className="profile-datas">
+              <div>
+              <h3>Name</h3>
+              <span>@</span>
+              </div>
+              <p>About</p>
+            </div>
+          </article>
+          <hr></hr>
+        </section>
+      </main>
     </div>
   );
 }
