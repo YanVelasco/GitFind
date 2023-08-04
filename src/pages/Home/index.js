705ffb36a748ad64c206723858e20399fa1f3354
Yan Velasco <yancarlosduartevelasco@gmail.com>
2023-08-03 21:09:05 -0300

    Finishi input and button

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,22 @@
 import { Header } from "../../components/Header";
+import backgroundImage from '../../assets/img/background.png';
 
+import './styles.css'
 function App() {
   return (
     <div className="App">
       <Header/>
+      <div className="content">
+        <img src = {backgroundImage} alt = "GitHub img" className="background-image"/>
+        <div className="Informations">
+          <div>
+            <input name="user" placeholder="@user" className ="user"/>
+            <button className="search">
+              Search
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
